b2406a8a8d2a3593a11d658d62969345e332f5dc
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-01-24 02:12:48 +0100

    Add DDL & Magnet logic

--- resources/views/livewire/components/add-ddl.blade.php
+++ resources/views/livewire/components/add-ddl.blade.php
@@ -1,42 +1,58 @@
 <?php
 
-use Livewire\Attributes\Validate; 
+use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 
 new class extends Component
 {
+    #[Validate('required')] 
+    public $url;
+
+	public $fileFieldId;
 
     /**
      * Mount the component.
      */
     public function mount(): void
     {
+		$this->fileFieldId = rand();
     }
 
     /**
      * Update the profile information for the currently authenticated user.
      */
-    public function add(): void
+    public function save(): void
     {
-        $this->dispatch('added');
+		if($this->url) {
+			$blackholeResult = app("BlackholeManager")->addDDL($this->url);
+
+			if($blackholeResult) {
+				$this->dispatch('saved');
+				$this->url = null;
+				$this->fileFieldId = rand();
+			} else {
+				$this->addError('url', 'Adding Magnet failed!');
+			}
+		}
     }
 
 
 }; ?>
 
 <section>
-    <form wire:submit="add" class="mt-4 space-y-4">
+    <form wire:submit="save" class="mt-4 space-y-4">
         <div>
-            <x-text-input wire:model="token" id="token" name="token" type="text" class="mt-1 block w-full" placeholder="https://" required />
-            <x-input-error class="mt-2" :messages="$errors->get('token')" />
+            <x-text-input wire:model="url" name="url" id="ddl{{ $fileFieldId }}" class="mt-1 block w-full h-10" placeholder="https://" required />
+            <x-input-error class="mt-2" :messages="$errors->get('url')" />
         </div>
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Add') }}</x-primary-button>
 
-            <x-action-message class="me-3" on="added">
+            <x-action-message class="me-3" on="saved">
                 {{ __('Added.') }}
             </x-action-message>
         </div>
     </form>
 </section>
+
--- resources/views/livewire/components/add-magnet.blade.php
+++ resources/views/livewire/components/add-magnet.blade.php
@@ -1,40 +1,56 @@
 <?php
 
-use Livewire\Attributes\Validate; 
+
+use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 
 new class extends Component
 {
+    #[Validate('required')] 
+    public $url;
+
+	public $fileFieldId;
 
     /**
      * Mount the component.
      */
     public function mount(): void
     {
+		$this->fileFieldId = rand();
     }
 
     /**
      * Update the profile information for the currently authenticated user.
      */
-    public function add(): void
+    public function save(): void
     {
-        $this->dispatch('added');
+		if($this->url) {
+			$blackholeResult = app("BlackholeManager")->addMagnet($this->url);
+
+			if($blackholeResult) {
+				$this->dispatch('saved');
+				$this->url = null;
+				$this->fileFieldId = rand();
+			} else {
+				$this->addError('url', 'Adding Magnet failed!');
+			}
+		}
     }
 
 
 }; ?>
 
 <section>
-    <form wire:submit="add" class="mt-4 space-y-4">
+    <form wire:submit="save" class="mt-4 space-y-4">
         <div>
-            <x-text-input wire:model="token" id="token" name="token" type="text" class="mt-1 block w-full" placeholder="magnet:xt=urn:btih:xxx" required />
-            <x-input-error class="mt-2" :messages="$errors->get('token')" />
+            <x-text-input wire:model="url" name="url" id="magnet{{ $fileFieldId }}" class="mt-1 block w-full h-10" placeholder="magnet:xt=urn:btih:xxx" required />
+            <x-input-error class="mt-2" :messages="$errors->get('url')" />
         </div>
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Add') }}</x-primary-button>
 
-            <x-action-message class="me-3" on="added">
+            <x-action-message class="me-3" on="saved">
                 {{ __('Added.') }}
             </x-action-message>
         </div>
