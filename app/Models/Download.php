91d969d1019b87bc97a011446035d348c9776cfb
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-01-24 22:59:30 +0100

    Update Download.php

--- app/Models/Download.php
+++ app/Models/Download.php
@@ -9,6 +9,16 @@ class Download extends Model
 {
     use HasFactory;
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Listen for the updating event and update the updated_at timestamp
+        static::updating(function ($download) {
+            $download->updated_at = now();
+        });
+    }
+
     public function files()
     {
         return $this->hasMany(DownloadFile::class);
