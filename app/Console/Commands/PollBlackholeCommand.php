3c2ded692744eb1f7b4ba16b7bc78846249d62ad
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-01-24 22:59:39 +0100

    Create PollBlackholeCommand.php

---
+++ app/Console/Commands/PollBlackholeCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class PollBlackholeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'poll:blackhole';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Poll the blackhole for new downloads.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Call your pollBlackhole function
+        $this->info('Polling blackhole...');
+
+        try {
+            app("DownloadManager")->pollBlackhole();
+            $this->info('Polling completed.');
+        } catch (\Throwable $th) {
+           Log::error("command poll:blackhole error:" . $th->getMessage());
+           $this->error($th->getMessage());
+        }
+    }
+}
