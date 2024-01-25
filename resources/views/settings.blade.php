149891c3cc92c44f4ee6df2da17e2eacafccb469
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-01-25 21:12:03 +0100

    Feature: Browser Magnet Protocol Handler

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -18,9 +18,12 @@
                     <livewire:pages.settings.accounts />
                 </div>
             </div>
+
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                <livewire:pages.settings.protocolHandler />
+                </div>
+            </div>
         </div>
-
     </div>
-
-
 </x-app-layout>
