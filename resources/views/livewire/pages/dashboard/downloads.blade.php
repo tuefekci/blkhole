9c4c6605d586fa582d5ce5c1bf34b53aea3e68a2
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-01-25 21:12:54 +0100

    Update downloads.blade.php

--- resources/views/livewire/pages/dashboard/downloads.blade.php
+++ resources/views/livewire/pages/dashboard/downloads.blade.php
@@ -14,8 +14,12 @@ new class extends Component
      */
     public function mount(): void
     {
-		$this->downloads = Download::latest('updated_at')->take(20)->get();
+		$this->refreshDownloads();
     }
+
+	public function refreshDownloads() {
+		$this->downloads = Download::latest('updated_at')->take(20)->get();
+	}
 
 	
 
@@ -27,13 +31,13 @@ new class extends Component
   <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
     <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
       <div class="overflow-hidden">
-        <table class="min-w-full text-center text-sm font-light text-gray-900 dark:text-gray-100">
+        <table class="min-w-full text-center text-sm font-light text-gray-900 dark:text-gray-100" wire:poll.visible="refreshDownloads">
           <thead class="border-b bg-neutral-800 font-medium dark:border-gray-700 dark:bg-gray-900 text-left">
             <tr>
               <th scope="col" class=" px-6 py-4">Name</th>
               <th scope="col" class=" px-6 py-4">Type</th>
-			  <th scope="col" class=" px-6 py-4">Progress</th>
 			  <th scope="col" class=" px-6 py-4">Status</th>
+			  <th scope="col" class=" px-6 py-4">Progress</th>
 			  <th scope="col" class=" px-6 py-4">Last Update</th>
               <th scope="col" class=" px-6 py-4">Actions</th>
             </tr>
@@ -42,8 +46,15 @@ new class extends Component
 
 		  	@foreach($downloads as $download)
 				<tr class="border-b dark:border-gray-700 text-left">
-					<td class="whitespace-nowrap px-6 py-4 font-medium">{{ $download->name }}</td>
+					<td class="whitespace-nowrap px-6 py-4 font-medium truncate" title="{{ $download->name }}">
+						@if(strlen($download->name) > 40)
+							{{ substr($download->name, 0, 40) }}...
+						@else
+							{{ $download->name }}
+						@endif
+					</td>
 					<td class="whitespace-nowrap px-6 py-4">{{ __($download->src_type) }}</td>
+					<td class="whitespace-nowrap px-6 py-4">{{ __(app("DownloadManager")->getStatusAsString($download->status)) }}</td>
 					<td class="whitespace-nowrap px-6 py-4">
 						<div class="w-full bg-neutral-200 dark:bg-neutral-600">
 							<div
@@ -52,11 +63,9 @@ new class extends Component
 								{{ __(app("DownloadManager")->getProgress($download->id)) }}%
 							</div>
 						</div>
-
 					</td>
-					<td class="whitespace-nowrap px-6 py-4">{{ __(app("DownloadManager")->getStatusAsString($download->status)) }}</td>
 					<td class="whitespace-nowrap px-6 py-4">{{ $download->updated_at }}</td>
-					<td class="whitespace-nowrap px-6 py-4">@mdo</td>
+					<td class="whitespace-nowrap px-6 py-4"></td>
 				</tr>
 			@endforeach
 
