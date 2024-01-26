aae71a938d71db7c4c935ae1a378c5797d67ca17
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-01-26 03:06:35 +0100

    Update protocolHandler.blade.php

--- resources/views/livewire/pages/settings/protocolHandler.blade.php
+++ resources/views/livewire/pages/settings/protocolHandler.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Settings') }}
+            {{ __('Browser Protocol Handler') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
