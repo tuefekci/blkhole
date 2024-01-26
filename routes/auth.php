e5a3f96e86da2aa80360ef6bb0372cbb68c66cb4
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-01-26 01:22:03 +0100

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -23,9 +23,12 @@ Route::middleware('auth')->group(function () {
     Volt::route('verify-email', 'pages.auth.verify-email')
         ->name('verification.notice');
 
+    /*
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
+    */
+
 
     Volt::route('confirm-password', 'pages.auth.confirm-password')
         ->name('password.confirm');
