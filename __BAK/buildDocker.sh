b73f11780ac8661a9cf1ae9549bc75861a87edb3
Giacomo Tüfekci <kontakt@tuefekci.de>
2024-02-14 23:32:19 +0100

    Remove _BAK

--- __BAK/buildDocker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ./web; taskset -c 4-7 npm run build
-cd ./../
-taskset -c 4-7 composer update
-taskset -c 4-7 docker build -t blkhole .
--- __BAK/perf.bat
+++
@@ -1,2 +0,0 @@
-autocannon -c 100 -d 5 -p 10 http://localhost:1337/
-pause
